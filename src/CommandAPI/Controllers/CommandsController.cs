5ee63b5fd4296882e21f7d8452bbaabce2a4c2b8
haj31621 <johannes.halbritter@st.oth-regensburg.de>
2021-02-20 18:10:54 +0100

    Added Model and Mock Repository

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -2,6 +2,8 @@
 
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 namespace CommandAPI.Controllers
 {
     //Route [controller] is name of controller: Commands[Controller]
@@ -10,10 +12,36 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+
+        private readonly ICommandAPIRepo _repository;
+        public CommandsController(ICommandAPIRepo repository)
         {
-            return new string[] { "this", "is", "hard", "coded" };
+            _repository = repository;
+        }
+        /*        [HttpGet]
+                public ActionResult<IEnumerable<string>> Get()
+                {
+                    return new string[] { "this", "is", "hard", "coded" };
+                }*/
+
+
+    
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+        var commandItem = _repository.GetCommandById(id);
+        if (commandItem == null)
+        {
+        return NotFound();
+        }
+        return Ok(commandItem);
         }
     }
 }
